fc6357358f301b425b501d673aa2dd76f951ab97
anyangw <66450689+anyangw@users.noreply.github.com>
2020-07-27 15:30:36 -0400

    Update acceleration_test0726.ino

--- acceleration_test0726.ino
+++ acceleration_test0726.ino
@@ -12,19 +12,36 @@ void setup() {
   Serial.begin(9600);
 }
 
-// Accelerate PWM 1ms -> 1.1ms in 10s, and stay at max rpm for 30s 
 
+// Accelerate PWM 1ms -> 1.1ms in 10s, and stay at max rpm for 30s and deaccelerate in 10 s, then wait for 30 s 
 void loop(){
-   int i;
+   int i=1001;
 
-   for (i = 1000; i <= 1101; i++){
+   for (i = 1001; i <= 1101; i++){
         myservo.writeMicroseconds(i);   
         delay(100);    
         }
 
-    if (i>1100)  
-    delay(30000);
+    if (i=1101){
+        myservo.writeMicroseconds(i);  
+        delay(30000);
+        i++;
+        }
+/* if do not need deacceleration control
+    if(i>1101){
+        myservo.writeMicroseconds(1000);   
+        delay(100000);
+        }
+*/
+   for (i = 1102; i >= 1000; i--){
+        myservo.writeMicroseconds(i);   
+        delay(100);    
+        }
 
+   if(i=1000)
+        delay(30000);
+       
+   // return 0;
 }
 
 
